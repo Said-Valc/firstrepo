b514e800510568288b26f77c5a7d8b6a40649d12
Said <said.valc@gmail.com>
2022-03-26 15:11:50 +0300

    release 2

--- for/99bottles.js
+++ for/99bottles.js
@@ -4,11 +4,10 @@
 function song(param){
 	let str = '';
 	for(let i = param; i >= 1; i--){
-		let letter = bottles(i, ['Бутылка', 'Бутылки', 'Бутылок'])
-			str += ` <${i}> ${letter} пива на стене
-				<${i}> ${letter} пива!
-				Возьми одну, пусти по кругу
-				<${i -1}> ${bottles(i -1, ['Бутылка', 'Бутылки', 'Бутылок'])} пива на стене!\n`;
+		let letter_bottle = bottles(i, ['Бутылка', 'Бутылки', 'Бутылок']);
+		str += ` <${i}> ${letter_bottle} пива на стене
+				 <${i}> ${letter_bottle} пива! Возьми одну, пусти по кругу
+				 <${i -1}> ${bottles(i -1, ['Бутылка', 'Бутылки', 'Бутылок'])} пива на стене!\n`;
 	}
 	return str;
 }
@@ -18,7 +17,5 @@ function bottles(number, titles) {
     return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
 }
 
-
-console.log(bottles(5, ['Бутылка', 'Бутылки', 'Бутылок']));
 
 console.log(song(99));
