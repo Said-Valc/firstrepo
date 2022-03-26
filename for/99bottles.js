4571cae462a555f997af4a3b1a494d21f2989c35
Said <said.valc@gmail.com>
2022-03-26 15:27:36 +0300

    release 3

--- for/99bottles.js
+++ for/99bottles.js
@@ -1,21 +1,21 @@
-
-
-
-function song(param){
-	let str = '';
-	for(let i = param; i >= 1; i--){
-		let letter_bottle = bottles(i, ['Бутылка', 'Бутылки', 'Бутылок']);
-		str += ` <${i}> ${letter_bottle} пива на стене
-				 <${i}> ${letter_bottle} пива! Возьми одну, пусти по кругу
-				 <${i -1}> ${bottles(i -1, ['Бутылка', 'Бутылки', 'Бутылок'])} пива на стене!\n`;
-	}
-	return str;
+let bs = {
+	title: 'The bottle song',
+	couplet: '99 бутылок пива на стене, 99 бутылок пива! Возьми одну, пусти по кругу, 98 бутылок пива на стене!',
+	param: 99,
+	song: function(){
+		let str = '';
+		for(let i = this.param; i >= 1; i--){
+		str += ` <${i}> ${this.assistant(i, this.letters)} пива на стене
+				 <${i}> ${this.assistant(i, this.letters)} пива! Возьми одну, пусти по кругу
+				 <${i -1}> ${this.assistant(i-1, this.letters)} пива на стене!\n`;
+		}
+		console.log(str);
+	},
+	assistant: (number, titles) =>{
+    	let cases = [2, 0, 1, 1, 1, 2];
+    	return titles[(number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5]];  
+	},
+	letters: ['Бутылка', 'Бутылки', 'Бутылок']
 }
-
-function bottles(number, titles) {  
-    cases = [2, 0, 1, 1, 1, 2];  
-    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
-}
-
-
-console.log(song(99));
+bs.param = 99;
+bs.song();
